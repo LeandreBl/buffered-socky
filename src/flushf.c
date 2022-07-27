7418cbd08391af1f4bf06f8878820947e3941471
Leandre Blanchard <leandre.bla@gmail.com>
2022-07-27 17:34:50 +0200

    [fix] Write was writing on the reader buffer instead of the writer

--- src/flushf.c
+++ src/flushf.c
@@ -2,11 +2,11 @@
 
 ssize_t buffered_socky_flushf(struct buffered_socky *bsocky, int flags)
 {
-    ssize_t ret = socky_send(&bsocky->socky, bsocky->buffy_reader.data, bsocky->buffy_reader.size, flags);
+    ssize_t ret = socky_send(&bsocky->socky, bsocky->buffy_writer.data, bsocky->buffy_writer.size, flags);
 
     if (ret < 0) {
         return -1;
     }
-    buffy_discard(&bsocky->buffy_reader, (size_t)ret);
+    buffy_discard(&bsocky->buffy_writer, (size_t)ret);
     return ret;
 }
--- src/write.c
+++ src/write.c
@@ -2,5 +2,5 @@
 
 ssize_t buffered_socky_write(struct buffered_socky *bsocky, const void *data, size_t size)
 {
-    return buffy_write(&bsocky->buffy_reader, data, size);
+    return buffy_write(&bsocky->buffy_writer, data, size);
 }
